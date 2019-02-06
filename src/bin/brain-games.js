74534132e53fc182f7350e7b90fa53a24d987259
anashi <n_lazareva@mail.ru>
2019-02-06 15:52:20 +0300

    brain-even game moved to separate file game/brain-even.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { askName } from '..';
+import { askName } from '../games/game-base';
 
 console.log('Welcome to the Brain Games!');
 askName();
---
+++ src/games/brain-even.js
@@ -0,0 +1,31 @@
+import * as game from './game-base';
+
+export default () => {
+  const winnerScore = 3;
+
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+
+  const username = game.askName();
+
+  let isWinner = true;
+
+  for (let i = 0; i < winnerScore; i++) {
+    const question = game.getRandomNumber(1, 1000);
+    const even = game.isEven(question);
+    const correctAnswer = even ? 'yes' : 'no';
+
+    game.askQuestion(question);
+    const answer = game.getAnswer();
+
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      isWinner = false;
+      break;
+    }
+  }
+
+  game.finishGame(isWinner, username);
+};
